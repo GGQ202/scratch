3b07a043ec193e3351bab05e7d1aada596ec1799
ggq202 <1410300624@qq.com>
2023-01-09 16:05:25 +0800

    0

---
+++ args.py
@@ -0,0 +1,34 @@
+import torch
+from transformers import WEIGHTS_NAME, BertConfig,get_linear_schedule_with_warmup,AdamW, BertTokenizer
+from models.bert_for_ner import BertCrfForNer
+
+
+class Args:
+    def __init__(self):
+        self.no_cuda = True
+        self.device = None
+        self.n_gpu = torch.cuda.device_count()
+        self.label_list = ["X", 'B-CONT', 'B-EDU', 'B-LOC', 'B-NAME', 'B-ORG', 'B-PRO', 'B-RACE', 'B-TITLE',
+                           'I-CONT', 'I-EDU', 'I-LOC', 'I-NAME', 'I-ORG', 'I-PRO', 'I-RACE', 'I-TITLE',
+                           'O', 'S-NAME', 'S-ORG', 'S-RACE', "[START]", "[END]"]
+        self.config_class = BertConfig
+        self.model_class = BertCrfForNer
+        self.tokenizer_class = BertTokenizer
+        self.task = "do_train"
+        self.train_batch_size = 8
+        self.num_train_epochs = None
+        self.model_name_or_path = 'prev_trained_model/bert-base-chinese'
+        self.data_dir = "./datasets/cner"
+        self.train_max_seq_length = 128
+        self.model_type = 'bert'
+        self.max_steps = -1
+        self.gradient_accumulation_steps = 1
+        self.num_train_epochs = 3
+        self.weight_decay = 0.01
+        self.learning_rate = 5e-5
+        self.crf_learning_rate = 5e-5
+        self.warmup_proportion = 0.1
+        self.warmup_steps = None
+        self.adam_epsilon =1e-8
+        self.max_grad_norm = 1.0
+
---
+++ main.py
@@ -0,0 +1,141 @@
+import torch
+from torch.utils.data import TensorDataset, RandomSampler, DataLoader, DistributedSampler
+from transformers import AdamW, get_linear_schedule_with_warmup
+from args import Args
+
+import args
+from callback.progressbar import ProgressBar
+from processor import Processor, convert_examples_to_features, collate_fn
+
+
+def load_and_cache_examples(args, tokenizer, data_type='train'):
+    label_list = args.label_list
+    processor = Processor()
+    if data_type == 'train':
+        examples = processor.get_train_examples(args.data_dir)
+    elif data_type == 'dev':
+        examples = processor.get_dev_examples(args.data_dir)
+    else:
+        examples = processor.get_test_examples(args.data_dir)
+    features = convert_examples_to_features(examples=examples,
+                                            tokenizer=tokenizer,
+                                            label_list=label_list,
+                                            max_seq_length=args.train_max_seq_length if data_type == 'train' \
+                                                else args.eval_max_seq_length,
+                                            cls_token_at_end=bool(args.model_type in ["xlnet"]),
+                                            pad_on_left=bool(args.model_type in ['xlnet']),
+                                            cls_token=tokenizer.cls_token,
+                                            cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
+                                            sep_token=tokenizer.sep_token,
+                                            # pad on the left for xlnet
+                                            pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
+                                            pad_token_segment_id=4 if args.model_type in ['xlnet'] else 0,
+                                            )
+    # Convert to Tensors and build dataset
+    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+    all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
+    all_lens = torch.tensor([f.input_len for f in features], dtype=torch.long)
+    dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_lens, all_label_ids)
+    return dataset
+
+
+def train(args, train_dataset, model, tokenizer):
+    """ Train the model """
+    train_sampler = RandomSampler(train_dataset)
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size,
+                                  collate_fn=collate_fn)
+    if args.max_steps > 0:
+        t_total = args.max_steps
+        args.num_train_epochs = args.max_steps // (len(train_dataloader) // args.gradient_accumulation_steps) + 1
+    else:
+        t_total = len(train_dataloader) // args.gradient_accumulation_steps * args.num_train_epochs
+    # Prepare optimizer and schedule (linear warmup and decay)
+    no_decay = ["bias", "LayerNorm.weight"]
+    bert_param_optimizer = list(model.bert.named_parameters())
+    crf_param_optimizer = list(model.crf.named_parameters())
+    linear_param_optimizer = list(model.classifier.named_parameters())
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in bert_param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay': args.weight_decay, 'lr': args.learning_rate},
+        {'params': [p for n, p in bert_param_optimizer if any(nd in n for nd in no_decay)],
+         'weight_decay': 0.0, 'lr': args.learning_rate},
+
+        {'params': [p for n, p in crf_param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay': args.weight_decay, 'lr': args.crf_learning_rate},
+        {'params': [p for n, p in crf_param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0,
+         'lr': args.crf_learning_rate},
+
+        {'params': [p for n, p in linear_param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay': args.weight_decay, 'lr': args.crf_learning_rate},
+        {'params': [p for n, p in linear_param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0,
+         'lr': args.crf_learning_rate}
+    ]
+    args.warmup_steps = int(t_total * args.warmup_proportion)
+    optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+    # optimizer = AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps,
+                                                num_training_steps=t_total)
+
+    global_step = 0
+    steps_trained_in_current_epoch = 0
+
+    tr_loss, logging_loss = 0.0, 0.0
+    model.zero_grad()
+    # seed_everything(args.seed)  # Added here for reproductibility (even between python 2 and 3)
+    pbar = ProgressBar(n_total=len(train_dataloader), desc='Training', num_epochs=int(args.num_train_epochs))
+    for epoch in range(int(args.num_train_epochs)):
+        pbar.reset()
+        pbar.epoch_start(current_epoch=epoch)
+        for step, batch in enumerate(train_dataloader):
+            # Skip past any already trained steps if resuming training
+            if steps_trained_in_current_epoch > 0:
+                steps_trained_in_current_epoch -= 1
+                continue
+            model.train()
+            batch = tuple(t.to(args.device) for t in batch)
+            inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[3]}
+            inputs["token_type_ids"] = (batch[2] if args.model_type in ["bert", "xlnet"] else None)
+            outputs = model(**inputs)
+            loss = outputs[0]  # model outputs are always tuple in pytorch-transformers (see doc)
+            if args.n_gpu > 1:
+                loss = loss.mean()  # mean() to average on multi-gpu parallel training
+            if args.gradient_accumulation_steps > 1:
+                loss = loss / args.gradient_accumulation_steps
+            loss.backward()
+            pbar(step, {'loss': loss.item()})
+            tr_loss += loss.item()
+            if (step + 1) % args.gradient_accumulation_steps == 0:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                optimizer.step()
+                scheduler.step()  # Update learning rate schedule
+                model.zero_grad()
+                global_step += 1
+        if 'cuda' in str(args.device):
+            torch.cuda.empty_cache()
+    return global_step, tr_loss / global_step
+
+
+def main():
+    args = Args()
+    args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+
+    label_list = args.label_list
+    id2label = {i: label for i, label in enumerate(label_list)}
+    label2id = {label: i for i, label in enumerate(label_list)}
+    num_labels = len(label_list)
+
+    config_class, model_class, tokenizer_class = args.config_class, args.model_class, args.tokenizer_class
+    config = config_class.from_pretrained(args.model_name_or_path, num_labels=num_labels)
+    tokenizer = tokenizer_class.from_pretrained(args.model_name_or_path, do_lower_case=False)
+    model = model_class.from_pretrained(args.model_name_or_path, config=config)
+    model.to(args.device)
+
+    if args.task == "do_train":
+        train_dataset = load_and_cache_examples(args, tokenizer, data_type='train')
+        global_step, tr_loss = train(args, train_dataset, model, tokenizer)
+
+
+if __name__ == "__main__":
+    main()
